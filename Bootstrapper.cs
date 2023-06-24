19962edab551aaffaccf55d11ffc923c3034b35c
Dmitry-Dem <demenchuk.dmitry03@gmaol.com>
2023-06-24 10:32:24 +0300

    MVVM Setup

---
+++ Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using CryptocurrencyWPFApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CryptocurrencyWPFApp
+{
+	public class Bootstrapper : BootstrapperBase
+	{
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+		protected override void OnStartup(object sender, StartupEventArgs e)
+		{
+			DisplayRootViewForAsync<ShellViewModel>();
+		}
+	}
+}
